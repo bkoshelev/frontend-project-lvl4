3c5463a458b40df2d28221f90089391cf1127c22
bkoshelev <brskoshelev@gmail.com>
2022-10-12 13:27:58 +0300

    подключаем rollbar через пакет

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
 import './utils/i18n';
 import 'react-toastify/dist/ReactToastify.css';
 
-import App from './App';
-import './index.css';
+const rollbarConfig = {
+  enabled: process.env.NODE_ENV === 'production',
+  accessToken: process.env.ROLLBAR_TOKEN,
+  environment: 'production',
+  server: {
+    branch: 'main',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <ToastContainer />
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <App />
+        <ToastContainer />
+      </ErrorBoundary>
+    </Provider>
   </React.StrictMode>,
 );
